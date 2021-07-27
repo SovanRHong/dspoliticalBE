3423da9b6bf64f490a157d71ad568a462ead0ee0
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-07-27 18:38:55 -0400

    edit some spelling errors in pseudocode

--- application.py
+++ application.py
@@ -1,9 +1,6 @@
 import models
 from dotenv import load_dotenv
 import os
-import sqlalchemy
-import bcrypt
-import jwt
 import requests
 from flask_cors import CORS
 from flask import Flask, request
@@ -26,17 +23,23 @@ def root():
     return { 'message': 'You hit the root route!' }
 app.route('/', methods=["GET"])(root)
 
+
 @app.route('/ALL',methods=['GET'])
 def all_Train():
     try:
         response = requests.get(f'{metro_url}ALL',params={'format':'json','API_KEY':api_key})
-        
-        print(response.json())
-        return{'message':'search successful',"trains_data":response.json()}
-            
-    except Exception as e:
-        return{'message':'error request all train unsuccessful'},401
+            # GET data from API
 
+        print(response.json())
+            # if response == success, EVAL and Print data 
+        return{'message':'search successful',"trains_data":response.json()}
+            # FORMAT message:
+                #   response{status:ok,data:[]} 
+                # SEND message to FE   
+    except Exception as e:
+        return{'message':'reached your allowed API limit'},429
+            # if error:
+            # response {error}
     finally:
         print('ALL trains routes is working')
 
@@ -44,16 +47,24 @@ def all_Train():
 def single_station(station_codes):
     try:
         response = requests.get(f'{metro_url}{station_codes}',params={'format':'json','API_KEY':api_key})
-
+            #GET data from API
         print(response)
+            # if response == success, EVAL and Print data 
         if response:
             print(response.json())
             return{'message':'search successful',"station_data":response.json()}
+               # FORMAT message:
+                # response{status:ok,data:[]} 
+                # SEND message to FE 
         else:
             return{'message':'There are no trains at this station',"station_data":{"Trains":[]}}
-      
+                 # FORMAT message:
+                    # response{status:error,data:[]} 
+                    # SEND message to FE 
     except Exception as e:
         return{'message':'error request unsuccessful'},401
+        # if error:
+            # response {error}
     
     finally:
         print('single station route is working')
